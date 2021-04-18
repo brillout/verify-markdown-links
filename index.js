2fb008e2d635fd14f2cfb42b3ad872806c24c050
Romuald Brillout <git@brillout.com>
2021-04-18 15:26:36 +0200

    verify dead links

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const remarkValidateLinksPath = require.resolve("remark-validate-links");
+const remarkLintNoDeadUrls = require.resolve("remark-lint-no-dead-urls");
 const remarkCliPkgPath = require.resolve("remark-cli/package.json");
 const { resolve, dirname } = require("path");
 
 Object.assign(module.exports, { verifyMarkdownLinks });
 
 function verifyMarkdownLinks() {
-  remark(["-u", remarkValidateLinksPath, process.cwd()]);
+  remark(["--use", remarkLintNoDeadUrls, '--use', remarkValidateLinksPath, process.cwd()]);
 }
 
 // We don't use remark's API because it behaves differently than the CLI
 function remark(args) {
   const pkg = require(remarkCliPkgPath);
-  const cliPath = require.resolve(resolve(dirname(remarkCliPkgPath), pkg.bin["remark"]));
+  const cliPath = require.resolve(
+    resolve(dirname(remarkCliPkgPath), pkg.bin["remark"])
+  );
   process.argv.push(...args);
   require(cliPath);
 }
